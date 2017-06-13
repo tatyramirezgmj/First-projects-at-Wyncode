6702fa22faf440ec36faa11a719d5a4f8cd4104d
tatyramirezgmj <tatiana@trwdesignsolutions.com>
2017-06-12 21:43:18 -0400

    Returns quick fox as the new object of a class

---
+++ quick_fox_return.rb
@@ -0,0 +1,9 @@
+class Animal
+  def jumped_over(attribute1, attribute2)
+  end
+end
+lazy_dog = "lazy dog"
+daisy_log= "daisy_log"
+
+quick_fox= Animal.new
+p quick_fox.jumped_over(lazy_dog, daisy_log)
